b42211bf4dd01f6cbf54321353ea672eb862b228
rreyfockandroid <rreyfockandroid@gmail.com>
2018-12-16 21:43:59 +0100

    .

--- mysite/myapp/url.py
+++ mysite/myapp/url.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('myapp.views', 
-	url(r'^hello/', 'hello', name = 'hello'),
-	url(r'^hello_x/', 'helloX', name = 'helloX'),
-	url(r'^morning/', 'morning', name = 'morning'),
-	# http://127.0.0.1:8000/myapp/article/123/
-	url(r'^article/(\d+)/', 'viewArticle', name = 'article'),
-	# http://127.0.0.1:8000/myapp/articles/03/2003/
-	url(r'^articles/(\d{2})/(\d{4})', 'viewArticles', name = 'articles'),
-)
+urlpatterns = patterns('myapp.views',
+                       url(r'^hello/', 'hello', name='hello'),
+                       url(r'^hello_x/', 'helloX', name='helloX'),
+                       url(r'^morning/', 'morning', name='morning'),
+                       # http://127.0.0.1:8000/myapp/article/123/
+                       url(r'^article/(\d+)/', 'viewArticle', name='article'),
+                       # http://127.0.0.1:8000/myapp/articles/03/2003/
+                       url(r'^articles/(\d{2})/(\d{4})', 'viewArticles', name='articles'),
+                       )
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,25 +1,34 @@
 from django.http import HttpResponse
 from datetime import datetime
+import django
+import platform
 
 def helloX(request):
-   text = "<h1>Witaj w mojej aplikacji!</h1>"
-   return HttpResponse(text)
+    dver = django.get_version()
+    pver = platform.python_version()
+    text = "<h1>Witaj w mojej aplikacji! Version, python: %s, django: %s</h1>"% (pver, dver)
+    return HttpResponse(text)
+
 
 def morning(request):
-	text = "Good morning"
-	return HttpResponse(text)
+    text = "Good morning"
+    return HttpResponse(text)
+
 
 def viewArticle(request, articleId):
-	text = "Wyswietl artykul numer: %s"%articleId
-	return HttpResponse(text)
+    text = "Wyswietl artykul numer: %s" % articleId
+    return HttpResponse(text)
+
 
 def viewArticles(request, month, year):
-	text = "Wyswietl artykul z: %s/%s"%(year, month)
-	return HttpResponse(text)
+    text = "Wyswietl artykul z: %s/%s" % (year, month)
+    return HttpResponse(text)
+
 
 from django.shortcuts import render
 
+
 def hello(request):
-	today = datetime.now().date()
-	infotext = 'nazywam sie alicja'
-	return render(request, "hello.html", {"today":today, "infotext":infotext})
+    today = datetime.now().date()
+    infotext = 'nazywam sie alicja'
+    return render(request, "hello.html", {"today": today, "infotext": infotext})
